37673e6a1a3f76c01446c76d9708672b74cf1b0f
Sofia Rios <sofia.rios@usm.cl>
2024-05-09 22:05:09 -0400

    test actualizadoo

--- tests/test_addRequest.py
+++ tests/test_addRequest.py
@@ -1,6 +1,7 @@
 import unittest
 import requests
 import json
+from unittest.mock import MagicMock, patch
 
 class addRequestTest(unittest.TestCase):
     valid_source_request_data = None
@@ -23,8 +24,10 @@ class addRequestTest(unittest.TestCase):
             'Monto': 10000,
             'Tasa': 5,
             'Plazo': 12,
-            'Categoria': 'A'
+            'Categoria': 'A',
+            'agenteComercial': 'Pepito Pepe'
         }
+        
         # Incomplete data request
         cls.invalid_source_request_data = {
             'nombre': '',
@@ -46,14 +49,31 @@ class addRequestTest(unittest.TestCase):
         del cls.valid_source_request_data
         del cls.invalid_source_request_data
 
-    def test_valid_request(self):
+
+    @patch('requests.post')
+    def test_valid_request(self, mock_post):
+        # Simulating a successful response from the API
+        response = MagicMock()
+        response.status_code = 200
+        response.content = json.dumps({'message': 'Success'}).encode('utf-8')
+        mock_post.return_value = response
+
         response = requests.post(self.base_url, json=self.valid_source_request_data)
         self.assertEqual(response.status_code, 200)
+        content = json.loads(response.content.decode('utf-8'))
+        print(content)
 
+    @patch('requests.post')
+    def test_invalid_request(self, mock_post):
+        # Simulating an error response from the API
+        response = MagicMock()
+        response.status_code = 400
+        response.content = json.dumps({'message': 'Error'}).encode('utf-8')
+        mock_post.return_value = response
 
-    def test_invalid_request(self):
         response = requests.post(self.base_url, json=self.invalid_source_request_data)
         self.assertEqual(response.status_code, 400)     
+   
 
 
 if __name__ == '__main__':
